b6199b95646623f9b057733ce76c549bfb1697bd
hn004 <harshalnikam123>
2020-12-03 16:03:08 +0530

    done with laglat multiple marker.

--- app/src/main/java/com/example/firebase01/MapActivity.java
+++ app/src/main/java/com/example/firebase01/MapActivity.java
@@ -10,10 +10,18 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 class MainActivity extends FragmentActivity implements OnMapReadyCallback {
     GoogleMap map;
+    ArrayList<LatLng>arrayList=new ArrayList<LatLng>();
+    LatLng Maharashtra=new LatLng(18.515625722387334, 73.85409690308553);
+    LatLng pimpri=new LatLng(18.650470685575502, 73.8230514782272);
+    LatLng nashik=new LatLng(20.0020753706489, 73.78785431640247);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,9 @@ class MainActivity extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        arrayList.add(Maharashtra);
+        arrayList.add(pimpri);
+        arrayList.add(nashik);
     }
 
 
@@ -30,9 +41,13 @@ class MainActivity extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map=googleMap;
-        LatLng Maharashtra=new LatLng(18.515625722387334, 73.85409690308553);
-        map.addMarker(new MarkerOptions().position(Maharashtra).title("maharashtra"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(Maharashtra));
+
+        for (int i=0;i<arrayList.size();i++){
+            map.addMarker(new MarkerOptions().position(arrayList.get(i)).title("Marker"));
+            map.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+            map.moveCamera(CameraUpdateFactory.newLatLng(arrayList.get(i)));
+        }
+
 
     }
 }
