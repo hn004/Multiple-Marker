bb604d395949a7a8d705796ab590056d4fc25618
hn004 <harshalnikam123>
2020-12-03 15:36:38 +0530

    done with laglat

--- app/src/main/java/com/example/firebase01/MapActivity.java
+++ app/src/main/java/com/example/firebase01/MapActivity.java
@@ -3,14 +3,36 @@ package com.example.firebase01;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map=googleMap;
+        LatLng Maharashtra=new LatLng(18.515625722387334, 73.85409690308553);
+        map.addMarker(new MarkerOptions().position(Maharashtra).title("maharashtra"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Maharashtra));
 
     }
 }
